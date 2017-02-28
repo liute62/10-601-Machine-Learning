92fbbadf864914e68ab90d05f006bf27f5a6dfc0
liute62 <liute62@sina.cn>
2017-02-28 12:21:07 -0500

    Initial hw4 code structures.

--- CMU/hw3/python/LinReg.py
+++ CMU/hw3/python/LinReg.py
@@ -1,6 +1,7 @@
 import os
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def LinReg_ReadInputs(filepath):
@@ -10,16 +11,31 @@ def LinReg_ReadInputs(filepath):
 
     #Input
     #filepath : The path where all the four csv files are stored.
-
     #output 
     #XTrain : NxK+1 numpy matrix containing N number of K+1 dimensional training features
     #yTrain : Nx1 numpy vector containing the actual output for the training features
     #XTest  : nxK+1 numpy matrix containing n number of K+1 dimensional testing features
     #yTest  : nx1 numpy vector containing the actual output for the testing features
-    
-    
+    XTrain = np.genfromtxt(os.path.join(filepath, 'LinReg_XTrain.csv'), delimiter=',')
+    yTrain = np.genfromtxt(os.path.join(filepath, 'LinReg_yTrain.csv'), delimiter=',')
+    XTest = np.genfromtxt(os.path.join(filepath, 'LinReg_XTest.csv'), delimiter=',')
+    yTest = np.genfromtxt(os.path.join(filepath, 'LinReg_yTest.csv'), delimiter=',')
+    for index in range(0,XTrain.shape[1]):
+        max_train = max(XTrain[0:XTrain.shape[0],index])
+        max_test = max(XTest[0:XTest.shape[0],index])
+        max_all = max(max_train,max_test)
+        min_train = min(XTrain[0:XTrain.shape[0], index])
+        min_test = min(XTest[0:XTest.shape[0], index])
+        min_all = min(min_train, min_test)
+        XTrain[:,index] = (XTrain[:,index] - min_all) / (max_all - min_all)
+        XTest[:,index] = (XTest[:,index] - min_all) / (max_all - min_all)
+    one_train = np.ones((XTrain.shape[0],1))
+    one_test = np.ones((XTest.shape[0],1))
+    XTrain = np.hstack((one_train,XTrain))
+    XTest = np.hstack((one_test,XTest))
     return (XTrain, yTrain, XTest, yTest)
-    
+
+
 def LinReg_CalcObj(X, y, w):
     
     #function that outputs the value of the loss function L(w) we want to minimize.
@@ -35,11 +51,16 @@ def LinReg_CalcObj(X, y, w):
     
     #Output
     #loss   : The value of the loss function we want to minimize
-    
+    # loss = np.zeros((X.shape[0],1))
     lossVal = 0
-    
+    y = y.reshape(y.shape[0],1)
+    list = np.subtract(np.dot(X,w),y)
+    sum = 0
+    for row in range(0,list.shape[0]):
+        item = list[row,0]
+        sum += item * item
+    lossVal = sum / X.shape[0]
     return lossVal
-
 
 
 def LinReg_CalcSG(x, y, w):
@@ -54,9 +75,17 @@ def LinReg_CalcSG(x, y, w):
 
     #Output
     #sg : gradient of the weight vector
-    
-    sg = 0
-    
+    # x * w (1 * 14) (14 * 1) - y
+    x_w = np.dot(x,w)
+    loss = x_w - y
+    sg = (loss * x).transpose() * 2
+    # #print tmp
+    # # (1 * 1)' * (1 * 14)
+    # tmp = tmp.reshape(1,1)
+    # x = x.reshape(1,x.shape[0])
+    # tmp2 = np.dot(tmp,x)
+    # # (1 * 14)
+    # sg = np.sum(tmp2)
     return sg
 
 def LinReg_UpdateParams(w, sg, eta):
@@ -71,7 +100,7 @@ def LinReg_UpdateParams(w, sg, eta):
 
     #Output
     #w  : Updated weight vector
-    
+    w = np.subtract(w,eta * sg)
     return w
     
 def LinReg_SGD(XTrain, yTrain, XTest, yTest):
@@ -88,16 +117,46 @@ def LinReg_SGD(XTrain, yTrain, XTest, yTest):
     #w    : Updated Weight vector after completing the stochastic gradient descent
     #trainLoss : vector of training loss values at each epoch
     #testLoss : vector of test loss values at each epoch
-    
     trainLoss = []
     testLoss = []
-    
+    w = np.zeros((XTrain.shape[1],1))
+    iter = 0
+    for i in range(0,w.shape[0]):
+        w[i,0] = 0.5
+    print w
+    for epoch in range(0,100):
+        #sg = np.zeros((XTrain.shape[1],1))
+        for sample in range(0,XTrain.shape[0]):
+            x_row = XTrain[sample,:]
+            x_row = x_row.reshape(1,x_row.shape[0])
+            y_row = yTrain[sample]
+            sg = LinReg_CalcSG(x_row,y_row,w)
+            w = LinReg_UpdateParams(w,sg,0.5 / math.sqrt(iter+1))
+            iter += 1
+        trainLoss.append(LinReg_CalcObj(XTrain,yTrain,w))
+        testLoss.append(LinReg_CalcObj(XTest,yTest,w))
     return (w, trainLoss, testLoss)
     
-def plot():     # This function's results should be returned via gradescope and will not be evaluated in autolab.
-    
+def plot(trainLoss,testLoss,is_test):
+    # This function's results should be returned via gradescope and will not be evaluated in autolab.
+    plt.plot(trainLoss,'')
+    plt.title('training losses versus the epoch.')
+    plt.xlabel('#epoch times')
+    plt.ylabel('trainLoss')
+    plt.show()
+    # plt.plot(testLoss)
+    # plt.title('test losses versus the epoch.')
+    # plt.xlabel('#epoch times')
+    # plt.ylabel('testLoss')
+    # plt.show()
     return None
-    
 
-    
-    
+#
+(XTrain, yTrain, XTest, yTest) = LinReg_ReadInputs('../data')
+# # # # lossVal = LinReg_CalcObj(XTrain,yTrain,np.ones((XTrain.shape[1],1)))
+# # # # print lossVal
+# # # #w = np.full((XTrain.shape[1],1),0.5)
+# # # #sg = LinReg_CalcSG(XTrain[0,:],yTrain[0],w)
+# # # #w2 = LinReg_UpdateParams(w,sg,0.1)
+(w , trainLost, testLoss) = LinReg_SGD(XTrain, yTrain, XTest, yTest)
+plot(trainLost,testLoss,1)
--- CMU/hw3/python/LogReg.py
+++ CMU/hw3/python/LogReg.py
@@ -1,6 +1,7 @@
 import os
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def LogReg_ReadInputs(filepath):
@@ -10,13 +11,19 @@ def LogReg_ReadInputs(filepath):
 
     #Input
     #filepath : The path where all the four csv files are stored.
-
+    XTrain = np.genfromtxt(os.path.join(filepath, 'LogReg_XTrain.csv'), delimiter=',')
+    yTrain = np.genfromtxt(os.path.join(filepath, 'LogReg_yTrain.csv'), delimiter=',')
+    XTest = np.genfromtxt(os.path.join(filepath, 'LogReg_XTest.csv'), delimiter=',')
+    yTest = np.genfromtxt(os.path.join(filepath, 'LogReg_yTest.csv'), delimiter=',')
     #output 
     #XTrain : Nx(K+1) numpy matrix containing N number of K+1 dimensional training features
     #yTrain : Nx1 numpy vector containing the actual output for the training features
     #XTest  : nx(K+1) numpy matrix containing n number of K+1 dimensional testing features
     #yTest  : nx1 numpy vector containing the actual output for the testing features
-    
+    one_train = np.ones((XTrain.shape[0], 1))
+    one_test = np.ones((XTest.shape[0], 1))
+    XTrain = np.hstack((one_train, XTrain))
+    XTest = np.hstack((one_test, XTest))
     return (XTrain, yTrain, XTest, yTest)
     
 def LogReg_CalcObj(X, y, w):
@@ -35,9 +42,16 @@ def LogReg_CalcObj(X, y, w):
     #Output
     #cll   : The conditional log likelihood we want to maximize
     
-    cll =  0
-    
-    return cll
+    y = y.reshape(y.shape[0], 1)
+    y_s = np.dot(X, w)
+    cll = 0.0
+    for row in range(0, y_s.shape[0]):
+        item = y_s[row, 0]
+        if y[row] == 1:
+            cll += math.log(sigmoid(item))
+        else:
+            cll += math.log(1 - sigmoid(item))
+    return cll / X.shape[0]
     
 def LogReg_CalcSG(x, y, w):
     
@@ -51,9 +65,9 @@ def LogReg_CalcSG(x, y, w):
 
     #Output
     #sg : gradient of the weight vector
-    
-    sg = 0
-    
+    x_w = np.dot(x, w)
+    loss = y - sigmoid(x_w)
+    sg = (loss * x).transpose()
     return sg
         
 def LogReg_UpdateParams(w, sg, eta):
@@ -68,7 +82,7 @@ def LogReg_UpdateParams(w, sg, eta):
 
     #Output
     #w  : Updated weight vector
-    
+    w = np.subtract(w,eta * sg)
     return w
     
 def LogReg_PredictLabels(X, y, w):
@@ -88,11 +102,20 @@ def LogReg_PredictLabels(X, y, w):
     #Output
     #yPred : An nx1 vector of the predicted labels for yTest/yTrain
     #perMiscl : The percentage of y's misclassified
-    
     yPred = []
-    perMiscl = 0
-    
-    return (yPred, PerMiscl)    
+    mis_label = 0
+    for row in range(0,X.shape[0]):
+        item = X[row,:]
+        item = item.reshape(1,X.shape[1])
+        p = sigmoid(np.dot(item,w))
+        if p > 0.5:
+            yPred.append(1)
+        else:
+            yPred.append(0)
+        if yPred[row] != y[row]:
+            mis_label += 1
+    perMiscl = mis_label * 1.0 / X.shape[0]
+    return (yPred, perMiscl)
 
 def LogReg_SGA(XTrain, yTrain, XTest, yTest):
     
@@ -111,10 +134,48 @@ def LogReg_SGA(XTrain, yTrain, XTest, yTest):
     #yPred         : a vector of your predictions for yTest using your final w
     
     trainPerMiscl = []
-    testPerMiscl = [] 
-    
+    testPerMiscl = []
+    w = np.zeros((XTrain.shape[1], 1))
+    iter = 0
+    for i in range(0, w.shape[0]):
+        w[i, 0] = 0.5
+    for epoch in range(0, 5):
+        for sample in range(0, XTrain.shape[0]):
+            x_row = XTrain[sample, :]
+            x_row = x_row.reshape(1, x_row.shape[0])
+            y_row = yTrain[sample]
+            sg = LogReg_CalcSG(x_row, y_row, w)
+            w = LogReg_UpdateParams(w, -sg, 0.5 / math.sqrt(iter + 1))
+            iter += 1
+            if iter % 200 == 0:
+                pred_train,perMiscl_train = LogReg_PredictLabels(XTrain,yTrain,w)
+                pred_test,perMiscl_test = LogReg_PredictLabels(XTest,yTest,w)
+                trainPerMiscl.append(perMiscl_train)
+                testPerMiscl.append(perMiscl_test)
+    yPred = LogReg_PredictLabels(XTest,yTest,w)
     return (w, trainPerMiscl, testPerMiscl, yPred)
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
     
-def plot():     # This function's results should be returned via gradescope and will not be evaluated in autolab.
-    
+def plot(train_error,test_error):
+    #This function's results should be returned via gradescope and will not be evaluated in autolab.
+    plt.plot(train_error)
+    plt.title("training % of misclassified points vs SGA per 200 iterations")
+    plt.xlabel('# / every 200 iterations')
+    plt.ylabel('percentage of mis-classification train data')
+    plt.show()
+    plt.title("testing % of misclassified points vs SGA per 200 iterations")
+    plt.plot(test_error)
+    plt.xlabel('# / every 200 iterations')
+    plt.ylabel('percentage of mis-classification test data')
+    plt.show()
     return None
+#
+(XTrain, yTrain, XTest, yTest) = LogReg_ReadInputs('../data')
+# #w = np.full((XTrain.shape[1],1),0.5)
+# #pred_train,error_train = LogReg_PredictLabels(XTrain,yTrain,w)
+# #cll = LogReg_CalcObj(XTrain,yTrain,np.ones((XTrain.shape[1],1)))
+(w,train,test,y) = LogReg_SGA(XTrain,yTrain,XTest,yTest)
+# # # print cll
+plot(train,test)
