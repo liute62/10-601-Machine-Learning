1578d83c00efddaa839cf384fa726383a3906e45
liute62 <liute62@sina.cn>
2017-03-03 13:19:49 -0500

    Add experiments scripts.

--- CMU/hw4/perceptron_starter_codes/python/hw4_script.py
+++ CMU/hw4/perceptron_starter_codes/python/hw4_script.py
@@ -30,6 +30,31 @@ plt.imshow(datapoint.reshape((28,28), order = 'F'), cmap='gray')
 # TODO: Test kernel_perceptron_predict function, defined in perceptron.py
 
 # TODO: Test kernel_perceptron_train function, defined in perceptron.py
-a0 = np.zeros(XTrain.shape[0])
-pe.kernel_perceptron_train(a0,XTrain,yTrain,10,0.001)
 # TODO: Run experiments outlined in HW4 PDF
+
+#1
+# a0 = np.zeros(XTrain.shape[1])
+# a1 = pe.perceptron_train(a0,XTrain,yTrain,10)
+# error_num = 0
+# for i in range(0,XTest.shape[0]):
+#     y_pre = pe.perceptron_predict(a1,XTest[i])
+#     y_real = yTest[i]
+#     if y_real != y_pre:
+#         error_num += 1
+# error_rate = error_num * 1.0 / XTest.shape[0]
+# print error_rate
+
+#2
+for sigma in (0.01,0.1,1,10,100,1000):
+    a0 = np.zeros(XTrain.shape[0])
+    a1 = pe.kernel_perceptron_train(a0,XTrain,yTrain,2,sigma)
+    error_num = 0;
+    for i in range(0, XTest.shape[0]):
+        y1 = pe.kernel_perceptron_predict(a1,XTrain,yTrain,XTest[i],sigma)
+        y2 = yTest[i]
+        if y1 != y2:
+            error_num += 1
+    error_rate = error_num * 1.0 / XTest.shape[0]
+    print sigma,error_rate
+
+#3
